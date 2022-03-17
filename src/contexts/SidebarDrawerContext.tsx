734b1b8c18a8bca337b1ba9e926a4df78cb5c863
Davidxy9 <davidwilliamlima@hotmail.com>
2022-03-16 21:06:06 -0300

    sidebar responsivo

---
+++ src/contexts/SidebarDrawerContext.tsx
@@ -0,0 +1,27 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
+
+interface SiderbarDrawerProviderProps {
+    children: ReactNode;
+}
+
+type SideBarDrawerContextData = UseDisclosureReturn
+
+const SiderbarDrawerContext = createContext({} as UseDisclosureReturn);
+
+export function SiderbarDrawerProvider({ children }: SiderbarDrawerProviderProps){
+    const discloure = useDisclosure()
+    const router = useRouter()
+
+    useEffect(() => {
+        discloure.onClose()
+    },[router.asPath])
+    return(
+        <SiderbarDrawerContext.Provider value={discloure}>
+            {children}
+        </SiderbarDrawerContext.Provider>
+    )
+}
+
+export const useSidebarDrawer = () => useContext(SiderbarDrawerContext)
